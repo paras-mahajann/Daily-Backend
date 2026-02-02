e40ede301c93df30348fa94733f284208f6cabba
paras-mahajann <mahajanparas912@gmail.com>
2026-02-02 22:07:02 +0530

    one method is remaining

---
+++ DAY-3/src/app.js
@@ -0,0 +1,32 @@
+//server create karna
+//server ko config karna
+
+const express = require('express')
+
+const app = express()//server create ho jata hai
+
+app.use(express.json())
+
+const notes = []
+
+app.post('/notes',(req,res)=>{    
+    notes.push(req.body);
+    console.log(notes);
+    
+    res.send("note created successfully.")
+})
+
+app.get('/notes',(req,res)=>{
+    res.send(notes);
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index];
+    res.send("note deleted successfully.")
+    console.log(notes);
+    
+    
+})
+
+
+module.exports = app
