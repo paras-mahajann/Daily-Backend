34fda442c59c3463bbc7dd4dfb65949b913940f0
paras-mahajann <mahajanparas912@gmail.com>
2026-02-13 21:31:08 +0530

    router done

--- DAY-8/server.js
+++ DAY-8/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const app = require('./src/app')
-const connectToDb = require('./src/config/database')
+const connectDb = require('./src/config/database')
 
-connectToDb()
 
-app.listen(3000,()=>{
+connectDb()
+
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
-    
+
 })
---
+++ DAY-8/src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.model')
+
+const authRouter = express.Router();
+
+authRouter.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+
+    const isUserAlreadyExist = await userModel.findOne({ email });
+    if (isUserAlreadyExist) {
+        return res.status(409).json({
+            message: "User already exist with this email address"
+        })
+    }
+
+    const user = await userModel.create({ name, email, password })
+
+
+    const token = jwt.sign(
+        {
+            id:user._id
+        },
+        process.env.JWT_SECRET
+    )
+
+
+    res.status(201).json({
+        message: "user register successfully.",
+        user
+    })
+
+
+
+})
+
+module.exports = authRouter;
