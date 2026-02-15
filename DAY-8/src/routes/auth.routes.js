f62b6d592c4225ee24300e567a103919a86aeaed
paras-mahajann <mahajanparas912@gmail.com>
2026-02-15 13:41:09 +0530

    implementing a login route and apply hashing on passwords

--- DAY-8/src/routes/auth.routes.js
+++ DAY-8/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const userModel = require('../models/user.model')
+const crypto = require('crypto')
 
 
 const authRouter = express.Router();
@@ -16,7 +17,9 @@ authRouter.post('/register', async (req, res) => {
         })
     }
 
-    const user = await userModel.create({ name, email, password })
+    const hash = crypto.createHash('md5').update(password).digest('hex');
+
+    const user = await userModel.create({ name, email, password:hash })
 
 
     const token = jwt.sign(
@@ -37,5 +40,41 @@ authRouter.post('/register', async (req, res) => {
 
 
 })
+
+authRouter.post('/protected',(req,res)=>{
+    const cookies = req.cookies;
+    res.status(200).json({
+        message:"you are on protected route..",
+        cookies
+    })
+})
+
+authRouter.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const user = await userModel.findOne({email});
+    if(!user){
+        return res.status(404).json({
+            message:"User not found with this email address"
+        })
+    }
+
+    if(user.password!==crypto.createHash('md5').update(password).digest('hex')){
+        return res.status(401).json({
+            message:"You Entered wrong password!!"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET);
+
+    res.cookie('jwt-token',token);
+
+    res.status(200).json({
+        message:"user logged in",
+        user
+    })
+})
+
 
 module.exports = authRouter;
