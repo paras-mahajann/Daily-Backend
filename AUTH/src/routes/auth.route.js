2d94b5833f4a1c4d50c746836d479521049e01e8
paras-mahajann <mahajanparas912@gmail.com>
2026-02-25 22:06:25 +0530

    authentication done

--- AUTH/src/routes/auth.route.js
+++ AUTH/src/routes/auth.route.js
@@ -35,5 +35,53 @@ authRouter.post('/register',async (req,res)=>{
 
 })
 
+authRouter.get('/get-me',async (req,res)=>{
+    const token = req.cookies.token;
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    console.log(decoded.id);
+    
+    const user = await userModel.findById(decoded.id);
+
+    res.json({
+        name:user.name,
+        email:user.email,
+    })
+    
+})
+
+authRouter.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+
+    const user = await userModel.findOne({email});
+    if(!user){
+        return res.status(404).json({
+            message:"User not found!"
+        })
+    }
+
+    const hash = crypto.createHash('sha256').update(password).digest('hex');
+    if(hash!==user.password){
+        return res.status(401).json({
+            message:"Invalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET,{expiresIn:'1h'});
+
+    res.cookie('token',token);
+
+    res.json({
+        message:"user logged in successfully.",
+        user:{
+            name:user.name,
+            email:user.email
+        }
+    })
+
+})
+
 
 module.exports = authRouter;
