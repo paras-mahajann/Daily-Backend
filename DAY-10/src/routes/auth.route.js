be57ce18e25c14529061bf55c8f93c460b96c17c
paras-mahajann <mahajanparas912@gmail.com>
2026-02-28 03:02:43 +0530

    controllers seperated

--- DAY-10/src/routes/auth.route.js
+++ DAY-10/src/routes/auth.route.js
@@ -1,110 +1,12 @@
 const express = require('express');
-const userModel = require('../models/user.model');
-const crypto = require('crypto')
-const jwt = require('jsonwebtoken')
+const authController = require('../controllers/auth.controllers')
 
 
 const authRouter = express.Router();
 
 
-authRouter.post('/register',async (req,res)=>{
-    const {username,email,password,bio,profileImage} = req.body;
+authRouter.post('/register',authController.registerController)
 
-    // const isUserExistByEmail = await userModel.findOne({email});
-    // if(isUserExistByEmail){
-    //     return res.status(409).json({
-    //         message:"User already exist with same email"
-    //     })
-    // }
-
-    // const isUserExistByUsername = await userModel.findOne({username});
-    // if(isUserExistByUsername){
-    //     return res.status(409).json({
-    //         message:"User is already exist with same username"
-    //     })
-    // }
-
-    const isUserExist = await userModel.findOne({
-        $or: [
-            {username},
-            {email}
-        ]
-    });
-
-    if(isUserExist){
-        return res.status(409).json({
-            message:"User already exist , " + (isUserExist.email == email ? "Email already exists":"User already exist")
-        })
-    }
-
-
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
-
-    const user = await userModel.create({username,email,password:hash,bio,profileImage});
-
-    const token = jwt.sign({
-        id:user._id,
-    },process.env.JWT_SECRET,{expiresIn:'1hr'});
-
-
-    res.cookie('token',token);
-
-    res.status(201).json({
-        message:"User Registered successfully.",
-        user:{
-            email:user.email,
-            username:user.username,
-            bio:user.bio,
-            profileImage:user.profileImage
-        }
-    })
-
-
-})
-
-authRouter.post('/login',async (req,res)=>{
-    const {username,email,password} = req.body;
-
-    const user = await userModel.findOne({
-        $or:[
-            {
-                username:username
-            },
-            {
-                email:email
-            }
-        ]
-    })
-
-    if(!user){
-        return res.status(404).json({
-            message:"user not found"
-        })
-    }
-
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
-
-    if(user.password !== hash){
-        return res.status(401).json({
-            message:"password invalid"
-        })
-    }
-
-    const token = jwt.sign({
-        id:user._id
-    },process.env.JWT_SECRET,{expiresIn:'1d'});
-
-    res.cookie('token',token);
-
-    res.status(200).json({
-        message:"User loggedIn successfully ",
-        user:{
-            username:user.username,
-            email:user.email,
-            bio:user.bio,
-            profileImage:user.profileImage
-        }
-    })
-})
+authRouter.post('/login',authController.loginController)
 
 module.exports = authRouter;
