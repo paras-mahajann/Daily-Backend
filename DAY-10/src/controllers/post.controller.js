185ad2ca96e6d559a0ff9c1dccf8b6dd9d8e8feb
paras-mahajann <mahajanparas912@gmail.com>
2026-03-06 23:02:11 +0530

    post-details and all-posts done

--- DAY-10/src/controllers/post.controller.js
+++ DAY-10/src/controllers/post.controller.js
@@ -52,7 +52,77 @@ async function createPostController(req,res){
     })
 }
 
+async function getPostController(req,res){
+
+    const token = req.cookies.token;
+    let decoded = null;
+   
+    try {
+        decoded = jwt.verify(token,process.env.JWT_SECRET);        
+    } catch (error) {
+        return res.status(401).json({
+            message:"Token invalid!"
+        })
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user:userId
+    });
+
+    res.status(200).json({
+        message:"post fetched successfully",
+        posts
+    })
+}
+
+async function getPostDetailsController(req,res) {
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized access"
+        })
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message:"Invalid Token"
+        })
+    }
+
+    const userId = decoded.id;
+
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId);
+
+    if(!post){
+        return res.status(404).json({
+            message:"Post not found."
+        })
+    }
+
+    const isValidUser = userId==post.user;
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden Content."
+        })
+    }
+
+    res.status(200).json({
+        message:"post fetched successfully",
+        post
+    })
+}
 
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- DAY-10/src/routes/post.route.js
+++ DAY-10/src/routes/post.route.js
@@ -7,6 +7,8 @@ const upload = multer({storage:multer.memoryStorage()});
 
 
 postRouter.post('/',upload.single("image"),postController.createPostController );
+postRouter.get('/',postController.getPostController);
+postRouter.get('/details/:postId',postController.getPostDetailsController)
 
 
 
