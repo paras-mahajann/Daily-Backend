697f05c895da4af5a5de03a5398087d86e16e478
paras-mahajann <mahajanparas912@gmail.com>
2026-03-12 23:01:02 +0530

    adding like route

--- DAY-10/src/controllers/user.controller.js
+++ DAY-10/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const followModel = require('../models/follow.model');
+const userModel = require('../models/user.model')
 
 
 async function followUserController(req,res) {
@@ -8,6 +9,16 @@ async function followUserController(req,res) {
     if(followerUsername==followeeUsername){
         return res.status(400).json({
             message:"You cannot follow yourself"
+        })
+    }
+
+    const isfolloweeExist = await userModel.findOne({
+        username:followeeUsername
+    });
+
+    if(!isfolloweeExist){
+        return res.status(400).json({
+            message:"User you are trying to follow does not exist"
         })
     }
 
@@ -36,6 +47,28 @@ async function followUserController(req,res) {
     
 }
 
+async function unfollowUserController(req,res) {
+    const followerUsername = req.user.username;
+    const followeeUsername = req.params.username;
+
+    const isUserFollowing = await followModel.findOne({
+        follower:followerUsername,
+        followee:followeeUsername
+    });
+
+    if(!isUserFollowing){
+        return res.status(200).json({
+            message:`You are not following ${followeeUsername}`
+        })
+    }
+
+    await followModel.findByIdAndDelete(isUserFollowing._id);
+
+    res.status(200).json({
+        message:`You have unfollowed ${followeeUsername}`
+    })
+}
 module.exports = {
-    followUserController
+    followUserController,
+    unfollowUserController
 }
--- DAY-10/src/routes/post.route.js
+++ DAY-10/src/routes/post.route.js
@@ -10,7 +10,6 @@ const identifyUser = require('../middlewares/auth.middleware');
 postRouter.post('/',upload.single("image"),identifyUser,postController.createPostController );
 postRouter.get('/',identifyUser,postController.getPostController);
 postRouter.get('/details/:postId',identifyUser,postController.getPostDetailsController)
-
-
+postRouter.post('/like/:postId',identifyUser,postController.likePostController);
 
 module.exports = postRouter;
