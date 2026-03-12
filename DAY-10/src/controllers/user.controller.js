c8ce306ef35e1d350a9af98c3b0b4b8b6e70f38c
paras-mahajann <mahajanparas912@gmail.com>
2026-03-12 21:36:53 +0530

    some daily work

--- DAY-10/src/controllers/user.controller.js
+++ DAY-10/src/controllers/user.controller.js
@@ -5,6 +5,34 @@ async function followUserController(req,res) {
     const followerUsername = req.user.username;
     const followeeUsername = req.params.username;
 
+    if(followerUsername==followeeUsername){
+        return res.status(400).json({
+            message:"You cannot follow yourself"
+        })
+    }
+
+    const isAlreadyFollowing = await followModel.findOne({
+        follower:followerUsername,
+        followee:followeeUsername
+    })
+
+    if(isAlreadyFollowing){
+        return res.status(200).json({
+            message:`You are already following ${followeeUsername}`,
+            follow:isAlreadyFollowing
+        })
+    }
+
+    const followRecord = await followModel.create({
+        follower:followerUsername,
+        followee:followeeUsername
+    })
+
+
+    res.status(201).json({
+        message:`You are now following ${followeeUsername}`,
+        follow:followRecord
+    })
     
 }
 
--- DAY-10/src/models/follow.model.js
+++ DAY-10/src/models/follow.model.js
@@ -3,20 +3,16 @@ const mongoose = require('mongoose')
 
 const followSchema = new mongoose.Schema({
     follower: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: [true, "Follower is required"]
+        type:String
     },
     followee: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: [true, "Followee is required"]
+        type:String
     }
 }, {
     timestamps:true
 })
 
 
-const followModel = mongoose.Model("follows",followSchema);
+const followModel = mongoose.model("follows",followSchema);
 
 module.exports = followModel;
