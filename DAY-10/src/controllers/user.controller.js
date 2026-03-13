f79c2f329aaa0e24950f55233d6494998acbdaf2
paras-mahajann <mahajanparas912@gmail.com>
2026-03-13 21:41:36 +0530

    adding feature of update status for follow request

--- DAY-10/src/controllers/user.controller.js
+++ DAY-10/src/controllers/user.controller.js
@@ -28,20 +28,34 @@ async function followUserController(req,res) {
     })
 
     if(isAlreadyFollowing){
+        if (isAlreadyFollowing.status === "rejected") {
+            isAlreadyFollowing.status = "pending";
+            await isAlreadyFollowing.save();
+            return res.status(200).json({
+                message:`Follow request resent to ${followeeUsername}`,
+                follow:isAlreadyFollowing
+            })
+        }
+        if (isAlreadyFollowing.status === "accepted") {
+            return res.status(200).json({
+                message:`You are already following ${followeeUsername}`,
+                follow:isAlreadyFollowing
+            })
+        }
         return res.status(200).json({
-            message:`You are already following ${followeeUsername}`,
+            message:`Follow request already sent to ${followeeUsername}`,
             follow:isAlreadyFollowing
         })
     }
 
     const followRecord = await followModel.create({
         follower:followerUsername,
-        followee:followeeUsername
+        followee:followeeUsername,
     })
 
 
     res.status(201).json({
-        message:`You are now following ${followeeUsername}`,
+        message:`Follow request sent to ${followeeUsername}`,
         follow:followRecord
     })
     
@@ -68,7 +82,42 @@ async function unfollowUserController(req,res) {
         message:`You have unfollowed ${followeeUsername}`
     })
 }
+
+async function updateFollowStatusController(req,res) {
+    const followeeUsername = req.user.username;
+    const followerUsername = req.params.username;
+    const { status } = req.body;
+
+    if(!['accepted','rejected'].includes(status)){
+        return res.status(400).json({
+            message:`status must be accepted or rejected`
+        })
+    }
+
+    const followRequest = await followModel.findOne({
+        follower:followerUsername,
+        followee:followeeUsername
+    })
+
+    if(!followRequest){
+        return res.status(400).json({
+            message:`follow request is not available for ${followerUsername}`
+        })
+    }
+
+    followRequest.status = status;
+    
+    await followRequest.save();
+
+    res.status(200).json({
+        message:`your follow request status updated by ${status} successfully.`,
+        followRequest
+    })
+
+
+}
 module.exports = {
     followUserController,
-    unfollowUserController
+    unfollowUserController,
+    updateFollowStatusController
 }
--- DAY-10/src/routes/user.route.js
+++ DAY-10/src/routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {followUserController,unfollowUserController} = require('../controllers/user.controller');
+const {followUserController,unfollowUserController, updateFollowStatusController} = require('../controllers/user.controller');
 const identifyUser = require('../middlewares/auth.middleware');
 
 const userRouter = express.Router();
@@ -8,7 +8,7 @@ userRouter.post('/follow/:username',identifyUser,followUserController);
 
 userRouter.post('/unfollow/:username',identifyUser,unfollowUserController);
 
-
+userRouter.post('/follow/:username/status',identifyUser,updateFollowStatusController)
 
 
 
