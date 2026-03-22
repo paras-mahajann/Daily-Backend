65b0d7132ed01116f635a1b9d57cd30bcad113c5
paras-mahajann <mahajanparas912@gmail.com>
2026-03-22 18:11:44 +0530

    register done

--- DAY-10/Frontend/src/features/auth/pages/Register.jsx
+++ DAY-10/Frontend/src/features/auth/pages/Register.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/form.scss'
 import { Link } from 'react-router'
+import {loading,user,handleRegister} from '../hooks/useAuth'
+import { useNavigate } from 'react-router'
 
 const Register = () => {
+
+    const [username,setUsername] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const navigate = useNavigate();
   
-    const handleSubmit = (e)=>{
+    const handleSubmit = async (e)=>{
            e.preventDefault();
+
+            await handleRegister(username,email,password);
+
+            navigate('/');
            
        }
    
@@ -15,9 +27,9 @@ const Register = () => {
            <div className="form-container">
                <h1>Register</h1>
                <form onSubmit={handleSubmit}>
-                   <input type="text" name='username' id='username' placeholder='Enter username'/>
-                   <input type="email" name='email' id='email' placeholder='Enter email' />
-                   <input type="password" name='password' id='password' placeholder='Enter password' />
+                   <input onInput={(e)=>{setUsername(e.target.value)}} type="text" name='username' id='username' placeholder='Enter username'/>
+                   <input onInput={(e)=>{setEmail(e.target.value)}} type="email" name='email' id='email' placeholder='Enter email' />
+                   <input onInput={(e)=>{setPassword(e.target.value)}} type="password" name='password' id='password' placeholder='Enter password' />
                    <button className='button primary-btn'>Register</button>
                </form>
                <p>Already have an account ? <Link to={'/login'}>Login.</Link></p>
