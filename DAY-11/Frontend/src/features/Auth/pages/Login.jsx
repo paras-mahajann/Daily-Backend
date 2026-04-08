367f7a05e7a8fb899b203231d1577e03bc6a29cc
paras-mahajann <mahajanparas912@gmail.com>
2026-04-08 22:43:56 +0530

    adding styles

--- DAY-11/Frontend/src/features/Auth/pages/Login.jsx
+++ DAY-11/Frontend/src/features/Auth/pages/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import '../style/login.scss'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+   <main className='login-page'>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' name='password' required />
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+   </main>
   )
 }
 
