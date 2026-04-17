832f4f97887847be9a09ad543bd24660dbe8146b
paras-mahajann <mahajanparas912@gmail.com>
2026-04-17 23:00:21 +0530

    emergency

--- DAY-11/Frontend/src/features/Auth/hooks/useAuth.js
+++ DAY-11/Frontend/src/features/Auth/hooks/useAuth.js
@@ -1,11 +1,1 @@
-import { useContext } from "react";
-import { AuthContext } from "../auth.context";
 
-
-
-export const useAuth = () => {
-    const  context = useContext(AuthContext);
-
-    const {user,setUser,loading,setLoading} = context;
-    
-}
