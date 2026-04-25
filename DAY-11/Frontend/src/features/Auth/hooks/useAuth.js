6fc239507682c2f95d30b768630a790e7b9c5853
paras-mahajann <mahajanparas912@gmail.com>
2026-04-25 22:33:58 +0530

    adding hook layer

--- DAY-11/Frontend/src/features/Auth/hooks/useAuth.js
+++ DAY-11/Frontend/src/features/Auth/hooks/useAuth.js
@@ -1,1 +1,36 @@
+import {login,register,getMe,logout} from "../services/auth.api"
+import { AuthContext } from "../auth.context"
+import { useContext } from "react"
 
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    const {user,setUser,loading,setLoading} = context;
+
+    async function handleRegister({username,email,password}) {
+        setLoading(true);
+        const data = await register({email,username,password});
+        setUser(data.user);
+        setLoading(false);
+    }
+
+    async function handleLogin({username,email,password}) {
+        setLoading(true);
+        const data = await login({email,password,username});
+        setUser(data.user);
+        setLoading(false)
+    }
+
+    async function handleGetMe() {
+        setLoading(true);
+        const data = await getMe();
+        setUser(data.user);
+        setLoading(false);
+    }
+    
+    async function handleLogout() {
+        setLoading(true);
+        const data = await logout();
+        setUser(null);
+        setLoading(false);
+    }
+}
